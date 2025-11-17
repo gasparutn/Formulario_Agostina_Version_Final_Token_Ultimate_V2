eab884b0060b7e5ff5debc6601ea816f02777ff2
Gaspar <gasparutn@gmail.com>
2025-11-17 18:30:08 -0300

    Esquema Precio_OK , comprobantes_OK_Token_OK, Hermanos_OK

---
+++ Sin título.js
@@ -0,0 +1,59 @@
+/*
+JORNADA NORMAL
+Socio= Si
+Método de Pago = Pago Efectivo (Adm del Club)
+Fila E59 = 1 Hermano/a: $550.00
+Fila E60 = 2 Hermanos/as: $992.00
+Fila E61 = 3 Hermanos/as: $1.402.500
+///////////////////////////////////
+JORNADA NORMAL
+Socio = Si
+Método de Pago = Pago Transferencia(Bancaria, Envíos dinero, Etc)
+Fila F56 = 1 Hermano/a: $590.000
+Fila F60 = 2 Hermanos/as: $1.062.000
+Fila F61 = 3 Hermanos/as: $1.530.000
+////////////////////////////////////
+JORNADA NORMAL
+Socio = NO 
+Método de Pago = Pago Efectivo (Adm del Club)
+Fila G59 = 1 Hermano/a: $600.000
+Fila G60 = 2 Hermanos/as: $1.080.000
+Fila G61 = 3 Hermanos/as: $
+///////////////////////////////////
+JORNADA NORMAL
+Socio = No
+Método de Pago = Pago Transferencia(Bancaria, Envíos dinero, Etc)(Bancaria, Envíos dinero)
+Fila H59 = 1 Hermano/a: $640.000
+Fila H60 = 2 Hermanos/as: $1.152.000
+Fila H61 = 3 Hermanos/as: $1.632.000
+////////////////////////////////////
+////////////////////////////////////
+JORNADA EXTENDIDA
+Socio = Si
+Método Pago = Efectivo
+Fila E69 = 1 Hermano/a: $650.000
+Fila E70 = 2 Hermanos/as: $1.170.000
+Fila E71 = 3 Hermanos/as: $1.657.000
+////////////////////////////////////
+JORNADA EXTENDIDA
+Socio = Si
+Método de Pago = Pago Transferencia(Bancaria, Envíos dinero, Etc)
+Fila F69 = 1 Hermano/a: $690.000
+Fila F70 = 2 Hermanos/as: $1.242.000
+Fila F71 = 3 Hermanos/as: $1.759.000
+////////////////////////////////////
+JORNADA EXTENDIDA
+Socio = No 
+Método de Pago = Pago Efectivo (Adm del Club)
+Fila G69 = 1 Hermano/a: $750.000
+Fila G70 = 2 Hermanos/as: $1.350.000
+Fila G71 = 3 Hermanos/as: $1.950.000
+////////////////////////////////////
+JORNADA EXTENDIDA
+Socio = No 
+Método de Pago = Pago Transferencia(Bancaria, Envíos dinero, Etc)
+Fila H69 = 1 Hermano/a: $790.000
+Fila H70 = 2 Hermanos/as: $1.442.000
+Fila H71 = 3 Hermanos/as: $2.014.000
+
+*/
